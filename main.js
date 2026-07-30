cf27e88d5ab1b021cc807d5741dec29e98cb0fb1
zippesquirrel <maxkinsberg@gmail.com>
2026-07-30 14:55:35 -0600

    clock backend, no text yet

--- main.js
+++ main.js
@@ -21,12 +21,116 @@ function store(key,e) {
     };
 };
 
-function createButtonArray(id,count){
-    let buttonArray = new Array;
-    for (i=1;i<=count;i++){
-        buttonArray.push(document.getElementById(`${id}-${i}`));
+function createClock(parentId,id,size){
+
+    // initial exception handling
+    if (typeof(size)!=="number"){
+        console.log(`Clock size ${size} is not a number!`);
+        return;
     };
-    return buttonArray;
+    if (size%2!==0||size<4||8<size){
+        console.log("Clock size is not 4,6 or 8")
+        return;
+    };
+    if (document.getElementById(id)!==null){
+        console.log("An element with that id already exists!")
+        return;
+    }
+
+
+    if (localStorage.getItem("int"+id)==null){
+        store("int"+id,0)
+    }
+    let localInt = parseInt(localStorage.getItem("int"+id));
+
+    const parent = document.getElementById(parentId)
+    const clockDiv = document.createElement("div")
+    
+    // Source - https://stackoverflow.com/a/17251714
+    // Posted by joelmdev, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-07-30, License - CC BY-SA 3.0
+    const sliceWidth = sliceAngle[(size-4)/2]
+
+    // pie slices
+    var div = 360 / size;
+    var radius = 5;
+    var parentdiv = clockDiv
+    var offsetToParentCenter = 0;//parseInt(parentdiv.offsetWidth / 2); //assumes parent is square
+    var offsetToChildCenter = 0;
+    var totalOffset = offsetToParentCenter - offsetToChildCenter;
+    for (var i = 1; i <= size; ++i) {
+        var childdiv = document.createElement('div');
+        childdiv.classList.add("pie","inactive")
+        childdiv.style.position = 'absolute';
+        var x = Math.sin(((div * i)-div) * (Math.PI / 180)+(Math.PI/size)) * radius;
+        var y = -Math.cos(((div * i)-div) * (Math.PI / 180)+(Math.PI/size)) * radius;
+        childdiv.id = `${id}-${i}`
+        childdiv.style.top = (y + totalOffset).toString() + "px";
+        childdiv.style.left = (x + totalOffset).toString() + "px";
+        childdiv.style.rotate = (-Math.atan2(x,y)*(180/Math.PI)-90).toString()+"deg";
+        childdiv.style.clipPath = `polygon(0% ${100*(0.5-sliceWidth/2)}%, 0% ${100*(0.5+sliceWidth/2)}%, 50% 50%)`;
+        parentdiv.appendChild(childdiv);
+    }
+    
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("clock-button-div");
+    
+    clockDiv.appendChild(buttonDiv)
+    clockDiv.id = id
+    clockDiv.classList.add("clock-parent-div")
+    parent.insertAdjacentElement("beforeend",clockDiv)
+    
+    window[id+"Array"] = createObjectArray(id,size)
+
+    const buttonUp = document.createElement("button");
+    buttonUp.id=id+"-up"
+    const buttonDn = document.createElement("button");
+    buttonDn.id=id+"-dn"
+    const buttonRm = document.createElement("button");
+    buttonRm.id=id+"-rm"
+    
+    buttonUp.classList.add("button-up","clock-button");
+    buttonUp.textContent="⬆️";
+    buttonUp.onclick = ()=>{
+        localInt = parseInt(localStorage.getItem("int"+id));
+        if (localInt<size){
+            buttonUpdate('int'+id,window[id+"Array"],localInt+1)
+        }
+    }
+    buttonDiv.appendChild(buttonUp);
+    buttonDn.classList.add("button-dn","clock-button");
+    buttonDn.textContent="⬇️";
+    buttonDn.onclick = ()=>{
+        localInt = parseInt(localStorage.getItem("int"+id));
+        if (localInt>0){
+            buttonUpdate('int'+id,window[id+"Array"],localInt-1)
+        }
+    }
+    buttonDiv.appendChild(buttonDn);
+    buttonRm.classList.add("button-rm","clock-button");
+    buttonRm.textContent="💀";
+    buttonRm.onclick = ()=>{
+        let proceed = confirm("are you sure?")
+        if (proceed){
+            localStorage.removeItem('int'+id)
+            clockDiv.replaceChildren()
+            clockDiv.remove()
+        }
+    }
+    buttonDiv.appendChild(buttonRm);
+
+    buttonDisplay(window[id+"Array"],localInt)
+};
+
+
+
+
+function createObjectArray(id,count){
+    let objectArray = new Array;
+    for (i=1;i<=count;i++){
+        objectArray.push(document.getElementById(`${id}-${i}`));
+    };
+    return objectArray;
 };
 
 function buttonDisplay(objectArray,int){
@@ -41,7 +145,7 @@ function buttonDisplay(objectArray,int){
     };
 }
 ;
-function buttonUpdate(key,objectArray, index,primary, secondary){
+function buttonUpdate(key,objectArray, index){
 
     let storedInt = localStorage.getItem(key);
 
@@ -66,7 +170,7 @@ function buttonUpdate(key,objectArray, index,primary, secondary){
     storedInt = localStorage.getItem(key);
     
     // update the appearance of the buttons
-    buttonDisplay(objectArray,storedInt,primary,secondary);
+    buttonDisplay(objectArray,storedInt);
 
 };
 
@@ -84,82 +188,21 @@ for (let i = 0; i < localStorage.length; i++){
     };
 };
 
-function createClock(parentId,size,id){
-    // initial exception handling
-    if (typeof(size)!=="number"){
-        console.log(`Clock size ${size} is not a number!`);
-        return;
-    };
-    if (size%2!==0||size<4||8<size){
-        console.log("Clock size is not 4,6 or 8")
-        return;
-    };
-    const parent = document.getElementById(parentId)
-    const clockDiv = document.createElement("div")
-    
-    // Source - https://stackoverflow.com/a/17251714
-    // Posted by joelmdev, modified by community. See post 'Timeline' for change history
-    // Retrieved 2026-07-30, License - CC BY-SA 3.0
-    const sliceWidth = sliceAngle[(size-4)/2]
 
-    // pie slices
-    var div = 360 / size;
-    var radius = 5;
-    var parentdiv = clockDiv
-    var offsetToParentCenter = 0;//parseInt(parentdiv.offsetWidth / 2); //assumes parent is square
-    var offsetToChildCenter = 0;
-    var totalOffset = offsetToParentCenter - offsetToChildCenter;
-    for (var i = 1; i <= size; ++i) {
-        var childdiv = document.createElement('div');
-        childdiv.classList.add("div2","inactive")
-        childdiv.style.position = 'absolute';
-        var x = Math.sin((div * i) * (Math.PI / 180)+(Math.PI/size)) * radius;
-        var y = Math.cos((div * i) * (Math.PI / 180)+(Math.PI/size)) * radius;
-        childdiv.id = `${id}-${i}`
-        childdiv.style.top = (y + totalOffset).toString() + "px";
-        childdiv.style.left = (x + totalOffset).toString() + "px";
-        childdiv.style.rotate = (-Math.atan2(x,y)*(180/Math.PI)-90).toString()+"deg";
-        childdiv.style.clipPath = `polygon(0% ${100*(0.5-sliceWidth/2)}%, 0% ${100*(0.5+sliceWidth/2)}%, 50% 50%)`
-        parentdiv.appendChild(childdiv);
-    }
-    
-    const buttonDiv = document.createElement("div")
-    buttonDiv.classList.add("clock-button-div")
-    const buttonUp = document.createElement("button")
-    const buttonDn = document.createElement("button")
-    const buttonRm = document.createElement("button")
-
-    buttonUp.classList.add("button-up","clock-button")
-    buttonUp.textContent="⬆️"
-    buttonUp.oninput = "buttonUpdate('intPlayerTrauma',traumaArray,1)"
-    buttonDiv.appendChild(buttonUp)
-    buttonDn.classList.add("button-dn","clock-button")
-    buttonDn.textContent="⬇️"
-    buttonDiv.appendChild(buttonDn)
-    buttonRm.classList.add("button-rm","clock-button")
-    buttonRm.textContent="💀"
-    buttonDiv.appendChild(buttonRm)
-
-
-    
-
-    clockDiv.appendChild(buttonDiv)
-    clockDiv.id = id
-    clockDiv.classList.add("clock-parent-div")
-    parent.insertAdjacentElement("beforeend",clockDiv)
-};
 
 
 
 
 
 //coin intial position
-const coinArray = createButtonArray("coin",4);
-const stressArray = createButtonArray("stress",9);
-const traumaArray = createButtonArray("trauma",4);
+const coinArray = createObjectArray("coin",4);
+const stressArray = createObjectArray("stress",9);
+const traumaArray = createObjectArray("trauma",4);
 
 buttonDisplay(coinArray,localStorage.getItem("intPlayerCoin"));
 buttonDisplay(stressArray,localStorage.getItem("intPlayerStress"));
 buttonDisplay(traumaArray,localStorage.getItem("intPlayerTrauma"));
 
-createClock("clock-div",6,"bob")
+createClock("clock-div","bob",4,)
+createClock("clock-div","tom",6,)
+createClock("clock-div","joe",8,)
